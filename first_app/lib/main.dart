1828ef448a5ade86cf279834f01e1d1c89d75fca
Paul <pauloladapo13@gmail.com>
2022-05-04 23:39:52 +0100

    More Basic tools

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,10 +1,22 @@
-import 'package:flutter/material.dart';
-void main() {
-  runApp(MyApp());  
-}
+// import 'package:flutter/material.dart';
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget  {
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-      return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('this is my default text!'),
+      ),
+    );
   }
 }
