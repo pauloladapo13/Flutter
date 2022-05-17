ddd02fd7161460e4fe1411740e1dd6c8935e8627
Paul <pauloladapo13@gmail.com>
2022-05-18 00:42:57 +0100

    17/05/2022

--- first_app/lib/answer.dart
+++ first_app/lib/answer.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-  final VoidCallback selectHandler;
+  final Function selectHandler;
   final String answerText;
-  //  if we attempt to return a value from the call
-  // function or pointer at the function
-  // final VoidCallback selectHandler; otherwise we use VoidCallback
+
   Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: ElevatedButton(
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Colors.blue),
-          foregroundColor: MaterialStateProperty.all(Colors.white),
-        ),
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
         child: Text(answerText),
         onPressed: selectHandler,
       ),
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,18 +1,16 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
 // void main() {
 //   runApp(MyApp());
 // }
-
-import 'package:flutter/material.dart';
-import './quiz.dart';
-import './result.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // ignore: todo
     // TODO: implement createState
     return _MyAppState();
   }
@@ -21,44 +19,80 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
-      'questionText': 'what\'s your favorite color',
-      'answers': ['Rabbit', 'Snake', 'Elelphant', 'Lion'],
-    },
-    {
-      'questionText': 'what\'s your favorite animal?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Max', 'Max', 'Max', 'Max'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
     setState(() {
-      _questionIndex += 1;
+      _questionIndex = 0;
+      _totalScore = 0;
     });
-    // ignore: avoid_print
-    print(_questionIndex);
+  }
 
-    // ignore: avoid_print
-    print('Answer chosen!');
+  void _answerQuestion(int score) {
+    // var aBool = true;
+    // aBool = false;
+
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];
+    // questions = []; // does not work if questions is a const
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('MyApp!'),
+          title: Text('My First App'),
         ),
         body: _questionIndex < _questions.length
             ? Quiz(
                 answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
                 questions: _questions,
-                questionIndex: _questionIndex)
-            : Result(),
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
